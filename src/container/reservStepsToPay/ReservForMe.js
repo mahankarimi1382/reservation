d0e98931f9b6ca5db6498d94a9bb16facbcf5f96
mahan karimi <13mahankarimi82@gmail.com>
2025-03-01 06:53:54 +0330

    fix responsive

--- src/container/reservStepsToPay/ReservForMe.js
+++ src/container/reservStepsToPay/ReservForMe.js
@@ -23,8 +23,8 @@ function ReservForMe({ setSteps }) {
       <div className=" flex justify-center items-center w-[90%] h-[124px] bg-[rgba(196,226,255,0.37)] rounded-2xl">
         <Image src={step1reservation} width={915} alt="step-picture" />
       </div>
-      <div className=" w-[90%] flex justify-between">
-        <div className=" w-[750px] p-4 h-[340px] gap-5 rounded-xl border border-[#005DAD] flex flex-col bg-[#F8FCFF]">
+      <div className=" w-[90%] flex flex-col gap-2 lg:flex-row justify-between">
+        <div className=" lg:w-[750px] p-4 lg:h-[340px] gap-5 rounded-xl border border-[#005DAD] flex flex-col bg-[#F8FCFF]">
           <h2 className=" pb-4 text-[20px] border-b border-dashed border-[#005DAD] text-[#005DAD] ">
             دریافت نوبت برای خودم
           </h2>
@@ -49,31 +49,31 @@ function ReservForMe({ setSteps }) {
           </div>
           <p>(باتوجه به بیمه تکمیلی شما هزینه تا 15% کسر می شود)</p>
         </div>
-        <div className=" w-[560px] p-4 h-[340px]  rounded-xl border border-[#005DAD] flex flex-col ">
+        <div className=" lg:w-[560px] p-4 h-[340px]  rounded-xl border border-[#005DAD] flex flex-col ">
           <div className=" border-b pb-4 border-[#CBCBCB] flex items-start justify-between">
             <div className=" flex justify-center items-center gap-2">
               <Image
                 alt="prof"
                 src={bahramProf}
                 width={87}
-                className="border rounded-full border-[#005DAD]"
+                className="border lg:w-[87px] w-[50px] rounded-full border-[#005DAD]"
               />
               <div className=" flex flex-col gap-2">
-                <h2 className=" text-[20px]">{doctorName}</h2>
-                <h5 className="text-[#757575]">{doctorSpecialties}</h5>
+                <h2 className=" text-xs lg:text-[20px]">{doctorName}</h2>
+                <h5 className=" text-xs lg:text-base text-[#757575]">{doctorSpecialties}</h5>
               </div>
             </div>
 
-            <p className="flex mt-3 items-center gap-1">
+            <p className=" lg:text-base text-xs flex mt-3 items-center gap-1">
               <Image width={22} src={star} alt="star" />
               4.5/5 از (نظر 320)
             </p>
           </div>
-          <div className=" flex py-5 text-[20px] gap-1 border-b border-[#CBCBCB]">
+          <div className=" flex lg:py-5 lg:text-[20px] gap-1 border-b border-[#CBCBCB]">
             <h2>تاریخ و ساعت نوبت:</h2>
             <h2 className=" text-[#757575]">{dateAndTime}</h2>
           </div>
-          <div className=" flex py-5 text-[20px] gap-1 border-b border-[#CBCBCB]">
+          <div className=" flex lg:py-5 lg:text-[20px] gap-1 border-b border-[#CBCBCB]">
             <h2>نوع نوبت دهی:</h2>
             <h2 className=" text-[#757575]">آنلاین، متنی</h2>
           </div>
@@ -85,7 +85,7 @@ function ReservForMe({ setSteps }) {
       </div>
       <button
         onClick={handleCompleteStep1}
-        className=" w-[460px] rounded-xl bg-[#005DAD] text-white py-4"
+        className=" px-10 lg:px-0 lg:w-[460px] rounded-xl bg-[#005DAD] text-white py-4"
       >
         ادامه
       </button>
