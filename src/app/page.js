ec204e00dfbba2bd20df69b2ee35f35f2f1aad22
mahan karimi <13mahankarimi82@gmail.com>
2025-03-10 10:39:11 +0330

    phoneMenu

--- src/app/page.js
+++ src/app/page.js
@@ -34,7 +34,7 @@ export default function Home() {
       <UserOpinions />
       <MagezineSection />
       <Footer />
-      {/* <PhoneMenu /> */}
+      <PhoneMenu />
       <SupportModal />
     </div>
   );
--- src/components/PhoneMenu.js
+++ src/components/PhoneMenu.js
@@ -1,27 +1,34 @@
 import React from "react";
-import HomeIcon from "../../public/Pics/home-icon.png";
+import HomeIcon from "../../public/Pics/phone-menu-icon/home_phone_icon.png";
+import clipboard from "../../public/Pics/phone-menu-icon/clipboard_phone_icon.png";
+import profile_phone from "../../public/Pics/phone-menu-icon/profile_phone_icon.png";
+import search_phone from "../../public/Pics/phone-menu-icon/search_phone_icon.png";
+
 import Image from "next/image";
 function PhoneMenu() {
   return (
-    <div className=" flex z-40 justify-around items-center w-full lg:hidden md:hidden h-[74px] fixed bottom-0 shadow-2xl bg-white text-white">
-      <div>
+    <div
+      className=" shadow-[0px_-9px_9px_0px_rgba(0,_0,_0,_0.1)]
+ flex z-40 justify-around items-center w-full lg:hidden md:hidden h-[74px] fixed bottom-0 bg-white text-white"
+    >
+      <div className=" w-1/4 flex items-center justify-center  flex-col">
         <Image width={24} src={HomeIcon} alt="home-icon" />
         <h2 className=" text-black">خانه</h2>
       </div>
-      <div>
+      <div className=" w-1/4 flex items-center justify-center  flex-col">
         {" "}
-        <Image width={24} src={HomeIcon} alt="home-icon" />
-        <h2 className=" text-black">خانه</h2>
+        <Image width={24} src={search_phone} alt="home-icon" />
+        <h2 className=" text-black">جستجو</h2>
       </div>
-      <div>
+      <div className=" w-1/4 flex items-center justify-center  flex-col">
         {" "}
-        <Image width={24} src={HomeIcon} alt="home-icon" />
-        <h2 className=" text-black">خانه</h2>
+        <Image width={24} src={clipboard} alt="home-icon" />
+        <h2 className=" text-black">نوبت های من</h2>
       </div>
-      <div>
+      <div className=" w-1/4 flex items-center justify-center  flex-col">
         {" "}
-        <Image width={24} src={HomeIcon} alt="home-icon" />
-        <h2 className=" text-black">خانه</h2>
+        <Image width={24} src={profile_phone} alt="home-icon" />
+        <h2 className=" text-black">پروفایل</h2>
       </div>
     </div>
   );
